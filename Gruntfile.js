b58859cb41d9d495551ab093dd87c1a7ab2965f3
Christobot <dxdstudio@gmail.com>
2014-06-05 17:41:23 -0700

    Add initial project files

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            css: {
+                files: ['sass/*.scss'],
+                tasks: ['sass:all']
+            }
+        },
+        sass: {
+            all: {
+                options: {
+                    style: 'compressed'
+                },
+                files: {
+                    'css/progress-wizard.min.css': 'sass/progress-wizard.scss'
+                }
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['sass', 'watch']);
+};
